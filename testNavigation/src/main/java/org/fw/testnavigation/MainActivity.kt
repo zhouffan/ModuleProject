afacba4beee3bd19bfaad706016515d32feb1f41
zhouffan <zhouffan@qq.com>
2021-02-24 23:22:08 +0800

    navigation

---
+++ testNavigation/src/main/java/org/fw/testnavigation/MainActivity.kt
@@ -0,0 +1,18 @@
+package org.fw.testnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+/**
+ * 学习笔记之Navigation
+ * https://www.jianshu.com/p/d45f7a69fd66
+ *
+ *
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
