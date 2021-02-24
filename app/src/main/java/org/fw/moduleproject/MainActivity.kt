7b99905efd91a3191dfb655d7299d1635a716a3d
zhouffan <zhouffan@qq.com>
2021-02-24 22:52:44 +0800

    启动页面站位图

--- app/src/main/java/org/fw/moduleproject/MainActivity.kt
+++ app/src/main/java/org/fw/moduleproject/MainActivity.kt
@@ -1,11 +1,17 @@
 package org.fw.moduleproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try {
+            Thread.sleep(5000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
     }
 }
